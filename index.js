d2259e0a4f03a90d5b051e146eb8d780fd558d16
Bernardo <moonbe77@gmail.com>
2020-05-22 15:01:15 +1000

    featuring input

---
+++ index.js
@@ -0,0 +1,8 @@
+let input = document.querySelector("#input");
+let btn = document.querySelector("#button");
+
+btn.addEventListener("onClick", btnClick);
+
+const btnClick = (e) => {
+  console.log(e);
+};
