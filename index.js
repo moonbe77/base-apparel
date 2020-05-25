591990dfedafb3b50c0ebf842990cbe395e172e7
Bernardo <moonbe77@gmail.com>
2020-05-25 18:41:42 +1000

    responsive still a lot of work to do

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let inputIconAlert = document.querySelector(".input-icon_alert");
 let inputWrapper = document.querySelector("#inputWrapper");
 let btn = document.querySelector("#button");
 let status = document.querySelector("#status");
+let modelImg = document.querySelector("#model-img");
+window.addEventListener("resize", changeImg);
 
 btn.addEventListener("click", (event) => {
   btnClick(event);
@@ -24,3 +26,14 @@ function validateEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+
+function changeImg(e) {
+  let w = window.innerWidth;
+
+  if (w < 921) {
+    modelImg.src = "./images/hero-mobile.jpg";
+  } else {
+    modelImg.src = "./images/hero-desktop.jpg";
+  }
+}
+changeImg();
